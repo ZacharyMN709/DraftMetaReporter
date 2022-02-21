be368d520257cd1b813a7edae62f819906c5493f
Zachary Northcott <zmnorthcott@mun.ca>
2022-02-21 02:25:21 -0330

    Separated RawDataWrapper into FramedData and helper functions.

---
+++ FramedData.py
@@ -0,0 +1,74 @@
+import pandas as pd
+
+import WUBRG
+from RawDataHandler import RawDataHandler
+
+class FramedData:   
+    def __init__(self, SET, FORMAT):
+        self._SET = SET
+        self._FORMAT = FORMAT
+        self._DATA = RawDataHandler(SET, FORMAT)
+        
+    
+    @property
+    def SET(self):
+        """The draft set."""
+        return self._SET
+    
+    @property
+    def FORMAT(self):
+        """The format type."""
+        return self._FORMAT
+    
+    @property
+    def DATA(self):
+        """The object which contains the data about the set and format."""
+        return self._DATA
+    
+    @property
+    def FUNCS(self):
+        """A helper object which contains convenience functions for plotting and subsetting."""
+        return self._FUNCS
+    
+    
+    def deck_group_frame(self, name=None, date=None, summary=False):
+        """Returns a subset of the 'GROUPED_ARCHTYPE' data as a DataFrame."""
+        if name is None: name = slice(None)
+        if date is None: date = slice(None)
+        
+        if summary:
+            return self.DATA.GROUPED_ARCHTYPE_SUMMARY_FRAME.loc(axis=0)[pd.IndexSlice[name]]
+        else:
+            return self.DATA.GROUPED_ARCHTYPE_HISTORY_FRAME.loc(axis=0)[pd.IndexSlice[date, name]]
+     
+    def deck_archetype_frame(self, deck_color=None, date=None, summary=False):
+        """Returns a subset of the 'SINGLE_ARCHTYPE' data as a DataFrame."""
+        if deck_color is None: deck_color = slice(None)
+        if type(deck_color) is str: deck_color = WUBRG.get_color_identity(deck_color)
+        if date is None: date = slice(None)
+            
+        if summary:
+            return self.DATA.SINGLE_ARCHTYPE_SUMMARY_FRAME.loc(axis=0)[pd.IndexSlice[deck_color]]
+        else:
+            return self.DATA.SINGLE_ARCHTYPE_HISTORY_FRAME.loc(axis=0)[pd.IndexSlice[date, deck_color]]
+    
+    def card_frame(self, name=None, deck_color=None, date=None, card_color=None, card_rarity=None, summary=False):
+        """Returns a subset of the 'CARD' data as a DataFrame."""
+        if name is None: name = slice(None)
+        if deck_color is None: deck_color = slice(None)
+        if date is None: date = slice(None)
+        if type(deck_color) is str: deck_color = WUBRG.get_color_identity(deck_color)
+        
+        if summary:
+            ret = self.DATA.CARD_SUMMARY_FRAME.loc(axis=0)[pd.IndexSlice[deck_color, name]]
+        else:
+            ret = self.DATA.CARD_HISTORY_FRAME.loc(axis=0)[pd.IndexSlice[date, deck_color, name]]
+            
+        if card_color:
+            color_set = WUBRG.get_color_subsets(WUBRG.get_color_identity(card_color))
+            ret = ret[ret['Color'].isin(list(color_set))]
+            
+        if card_rarity:
+            ret = ret[ret['Rarity'].isin(list(card_rarity))]
+        
+        return ret
