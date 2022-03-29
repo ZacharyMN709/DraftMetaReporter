4564cb84b1893c3eccddc370429e91f8b16d06ba
Zachary Northcott <zmnorthcott@mun.ca>
2022-03-28 21:55:56 -0230

    Small bug fixes, starting work on saving graphs as images.

--- settings.py
+++ settings.py
@@ -6,6 +6,13 @@ DATA_DIR_LOC = '..'
 
 # Graphing Defaults
 ROLL = 3
-GRAPH_DIR_NAME = 'Images'
-GRAPH_DIR_LOC = '.'
-
+GRAPH_DIR_NAME = 'Graphs'
+GRAPH_DIR_LOC = '..'
+DPI = 300
+ACCREDIT_KWARGS = {
+    'ha': 'center',
+    'va': 'bottom',
+    'weight': 'demi',
+    'size': 'medium',
+    'style': 'oblique'
+}
