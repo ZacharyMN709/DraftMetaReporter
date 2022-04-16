0eaeb3e288941ee9814f601b0330fa5b3fb22a5a
Zachary Northcott <zmnorthcott@mun.ca>
2022-04-16 14:18:11 -0230

    Moving helper classes from Notebook to main code.

--- data_fetching/FramedData.py
+++ data_fetching/FramedData.py
@@ -14,47 +14,25 @@ class FramedData:
     examined over certain parts of a format (After week 2, first meta shift, under-drafted colour becomes good, etc.)
     """
 
-    def __init__(self, set_name: str, format_name: str, load_summary: bool = True, load_history: bool = True):
-        self._set = set_name
-        self._format = format_name
-        self._data = RawDataHandler(set_name, format_name)
-        self._compare_key = SetMetadata.get_metadata(set_name).COMPARE_KEY
+    def __init__(self, set_code: str, format_name: str, load_summary: bool = True, load_history: bool = True):
+        self._set_metadata = SetMetadata.get_metadata(set_code)
+        self.SET = set_code
+        self.SET_NAME = self._set_metadata.FULL_NAME
+        self.FORMAT = format_name
+        self.FORMAT_ALIAS = FORMAT_NICKNAMES[self.FORMAT].upper()
+        self.DATA = RawDataHandler(set_code, format_name)
+        self._compare_key = self._set_metadata.COMPARE_KEY
 
         self.load_summary = load_summary
         self.load_history = load_history
 
-    @property
-    def set(self) -> str:
-        """The draft set."""
-        return self._set
-
-    @property
-    def full_set(self) -> str:
-        """The full name of the draft set."""
-        return SetMetadata.get_metadata(self.set).FULL_NAME
-
-    @property
-    def format(self) -> str:
-        """The format type."""
-        return self._format
-
-    @property
-    def format_shorthand(self) -> str:
-        """The shorthand of the format type."""
-        return FORMAT_NICKNAMES[self._format].upper()
-
-    @property
-    def data(self) -> RawDataHandler:
-        """The object which contains the data about the set and format."""
-        return self._data
-
     def check_for_updates(self) -> None:
         """Populates and updates all data properties, filling in missing data."""
-        self._data.check_for_updates()
+        self.DATA.check_for_updates()
 
     def reload_data(self) -> None:
         """Populates and updates all data properties, reloading all data."""
-        self._data.reload_data()
+        self.DATA.reload_data()
 
     # TODO: Create a function which takes in common parameters for subsetting the frames, and converts them
     #  into a kwarg dict of slices.
@@ -65,9 +43,9 @@ class FramedData:
         if date is None: date = slice(None)
 
         if summary:
-            return self.data.GROUPED_ARCHETYPE_SUMMARY_FRAME.loc(axis=0)[pd.IndexSlice[name]]
+            return self.DATA.GROUPED_ARCHETYPE_SUMMARY_FRAME.loc(axis=0)[pd.IndexSlice[name]]
         else:
-            return self.data.GROUPED_ARCHETYPE_HISTORY_FRAME.loc(axis=0)[pd.IndexSlice[date, name]]
+            return self.DATA.GROUPED_ARCHETYPE_HISTORY_FRAME.loc(axis=0)[pd.IndexSlice[date, name]]
 
     def deck_archetype_frame(self, deck_color=None, date=None, summary=False) -> pd.DataFrame:
         """Returns a subset of the 'SINGLE_ARCHETYPE' data as a DataFrame."""
@@ -76,9 +54,9 @@ class FramedData:
         if date is None: date = slice(None)
 
         if summary:
-            return self.data.SINGLE_ARCHETYPE_SUMMARY_FRAME.loc(axis=0)[pd.IndexSlice[deck_color]]
+            return self.DATA.SINGLE_ARCHETYPE_SUMMARY_FRAME.loc(axis=0)[pd.IndexSlice[deck_color]]
         else:
-            return self.data.SINGLE_ARCHETYPE_HISTORY_FRAME.loc(axis=0)[pd.IndexSlice[date, deck_color]]
+            return self.DATA.SINGLE_ARCHETYPE_HISTORY_FRAME.loc(axis=0)[pd.IndexSlice[date, deck_color]]
 
     def card_frame(self, name=None, deck_color=None, date=None, card_color=None, card_rarity=None,
                    summary=False) -> pd.DataFrame:
@@ -89,9 +67,9 @@ class FramedData:
         if isinstance(deck_color, str): deck_color = get_color_identity(deck_color)
 
         if summary:
-            ret = self.data.CARD_SUMMARY_FRAME.loc(axis=0)[pd.IndexSlice[deck_color, name]]
+            ret = self.DATA.CARD_SUMMARY_FRAME.loc(axis=0)[pd.IndexSlice[deck_color, name]]
         else:
-            ret = self.data.CARD_HISTORY_FRAME.loc(axis=0)[pd.IndexSlice[date, deck_color, name]]
+            ret = self.DATA.CARD_HISTORY_FRAME.loc(axis=0)[pd.IndexSlice[date, deck_color, name]]
 
         if card_color:
             color_set = get_color_subsets(get_color_identity(card_color))
---
+++ data_graphing/PlotterHelper.py
@@ -0,0 +1,93 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+from data_fetching import FramedData
+
+from data_graphing.utils import settings
+from data_graphing.ColorHandler import ColorHandler
+
+
+class PlotterHelper:
+    def __init__(self, data: FramedData, palette=None, color_dict=None):
+        self.DATA = data
+        self.FIG = None
+        self.AX = None
+        self.COLORS = ColorHandler(palette, color_dict)
+        os.makedirs(self.get_folder_path(), exist_ok=True)
+
+    def _get_sub_ax(self, x=None, y=None):
+        if not isinstance(self.AX, np.ndarray):
+            return self.AX
+        elif self.AX.ndim == 1:
+            return self.AX[x if x is not None else y]
+        elif self.AX.ndim == 2:
+            return self.AX[x, y]
+        else:
+            raise Exception('Unknown axes format.')
+
+    def get_folder_path(self):
+        return os.path.join(settings.GRAPH_DIR_LOC, settings.GRAPH_DIR_NAME, self.DATA.SET, self.DATA.FORMAT)
+
+    def get_file_path(self, filename):
+        return os.path.join(self.get_folder_path(), filename)
+
+    def new_single_plot(self, title, width=8, height=6, fontsize=settings.TITLE_SIZE):
+        self.FIG, self.AX = plt.subplots(1, 1)
+        self.FIG.set_size_inches(width, height)
+        self.AX.set_title(f"{self.DATA.SET} {self.DATA.FORMAT_ALIAS}: {title}", fontsize=fontsize)
+        self.set_x_axis_weekly()
+        return self.FIG, self.AX
+
+    def new_quad_plot(self, title, width=12, height=8, fontsize=settings.TITLE_SIZE):
+        self.FIG, self.AX = plt.subplots(2, 2)
+        self.FIG.set_size_inches(width, height)
+        plt.figtext(0.4, 0.9, f"{self.DATA.SET} {self.DATA.FORMAT_ALIAS}: {title}", fontsize=fontsize)
+        self.set_x_axis_weekly(g_x=0, g_y=0)
+        self.set_x_axis_weekly(g_x=0, g_y=1)
+        self.set_x_axis_weekly(g_x=1, g_y=0)
+        self.set_x_axis_weekly(g_x=1, g_y=1)
+        return self.FIG, self.AX
+
+    def accredit(self, y=0.01):
+        plt.figtext(0.5, y, settings.ACCREDIT_STR, **settings.ACCREDIT_KWARGS)
+
+    def desc_note(self, colors=None, roll=1, y=0.95):
+        col_filt = f"Color Filter: {colors}"
+        rol_filt = f"Rolling Average: {roll} Days"
+
+        if colors and roll > 1:
+            txt = f"{rol_filt}\n{col_filt}"
+        elif colors:
+            txt = f"\n{col_filt}"
+        elif roll > 1:
+            txt = f"\n{rol_filt}"
+        else:
+            txt = ""
+
+        plt.figtext(0.5, y, txt, **settings.FILTER_KWARGS)
+
+    def set_labels(self, x_label="", y_label="", fontsize=settings.LABEL_SIZE, g_x=None, g_y=None):
+        ax = self._get_sub_ax(g_x, g_y)
+        ax.set_xlabel(x_label, fontsize=fontsize)
+        ax.set_ylabel(y_label, fontsize=fontsize)
+
+    def set_data(self, data, col_list, inv_y=False, inv_x=False, g_x=None, g_y=None):
+        ax = self._get_sub_ax(g_x, g_y)
+        for col in col_list:
+            ax.plot(data.index, data[[col]], label=col, color=self.COLORS.get_color(col))
+        if inv_y: ax.invert_yaxis()
+        if inv_x: ax.invert_xaxis()
+        ax.legend()
+
+    def set_x_axis_weekly(self, g_x=None, g_y=None):
+        ax = self._get_sub_ax(g_x, g_y)
+        weeks = mdates.DayLocator(interval=7)
+        ax.xaxis.set_major_locator(weeks)
+        days = mdates.DayLocator(interval=1)
+        ax.xaxis.set_minor_locator(days)
+
+    def save_fig(self, filename, dpi=None):
+        if dpi is None: dpi = settings.DPI
+        self.FIG.savefig(self.get_file_path(filename.replace(' ', '_')), dpi=dpi)
