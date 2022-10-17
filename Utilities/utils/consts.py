6af934f1856f50f959e7280b1b1fec1d6bcef601
Zachary Northcott <zmnorthcott@mun.ca>
2022-10-16 22:48:50 -0230

    Removing unused consts file, and adding files for future caching work.

--- Utilities/utils/consts.py
+++
@@ -1,6 +0,0 @@
-from typing import Union
-
-# Typing Consts
-CARD_DATA = list[dict[str, Union[str, int, float]]]
-META_DATA = list[dict[str, Union[str, int, bool]]]
-WUBRG_CARD_DATA = dict[str, CARD_DATA]
