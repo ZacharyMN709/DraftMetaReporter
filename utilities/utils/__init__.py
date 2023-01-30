389409da30798660b9600d9f7d6fb1a328716dfe
Zachary Northcott <zmnorthcott@mun.ca>
2023-01-29 23:53:23 -0330

    Updating __init__.pys of `data_fetching`.
     - Removed duplicate settings in `utilities`.

--- utilities/utils/__init__.py
+++ utilities/utils/__init__.py
@@ -6,7 +6,7 @@ from utilities.utils.funcs import *
 from utilities.utils.settings import *
 
 
-from_funcs = ['ENCODING', 'DATA_DIR_NAME', 'DATA_DIR_LOC']
+from_funcs = ['ENCODING']
 
 from_settings = ['flatten_lists', 'load_json_file', 'save_json_file']
 
--- utilities/utils/settings.py
+++ utilities/utils/settings.py
@@ -1,4 +1,2 @@
 # File Location Defaults
 ENCODING = 'utf-8'
-DATA_DIR_NAME: str = '17LandsData'
-DATA_DIR_LOC: str = r'C:\Users\Zachary\Coding\GitHub'
