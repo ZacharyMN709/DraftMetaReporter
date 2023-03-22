2c17e7916ad12cf2adbbc2a9e0615f965546ca98
Zachary Northcott <zmnorthcott@mun.ca>
2023-03-21 23:50:33 -0230

    Setting up boilerplate of frame_display.

--- core/utilities/notebook_setups/frame_display.py
+++ core/utilities/notebook_setups/frame_display.py
@@ -1,19 +1,26 @@
+from core.utilities.notebook_setups.frame_tools import *
+from core.utilities.notebook_setups.frame_tools import __imports as __sub_imports
+from core.utilities.notebook_setups.frame_tools import set_up as sub_set_up
+
 import dataframe_image as dfi
 from scipy.stats import norm
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-from datetime import datetime
 
+# TODO: Make this and the other setup functions use an abstract base class, and layer
+#  each version on top  of the previous.
 from core.utilities.auto_logging import LogLvl, set_log_level, logging
-import core.wubrg
-
-from core.game_metadata import SETS
-from core.game_metadata import Card, CardManager, SetMetadata, FormatMetadata
-from core.data_fetching import DataLoader, LoadedData, DataFramer, FramedData, SetManager, CentralManager
-from core.data_fetching.utils import get_name_slice, get_color_slice, get_date_slice, STAT_COL_NAMES, \
-    rarity_filter, cmc_filter, card_color_filter, cast_color_filter, compose_filters
-from core.data_fetching.utils import DATA_DIR_LOC, DATA_DIR_NAME
 
 __imports = []
 __funcs = []
-__all__ = __imports + __funcs
+__all__ = __imports + __funcs + __sub_imports
+
+
+def info_splash() -> None:
+    pass
+
+
+def set_up(log_lvl=LogLvl.DEBUG, target_set=SETS[0], load_all=False) -> tuple[CentralManager, SetManager]:
+    _main, _set = sub_set_up(log_lvl, target_set, load_all)
+    info_splash()
+    return _main, _set
