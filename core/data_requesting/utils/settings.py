ed6882869947c98457d7129b51344a996a385fd7
Zachary Northcott <zmnorthcott@mun.ca>
2023-04-14 22:44:47 -0230

    Update settings.py
     - Reducing wait on success.

--- core/data_requesting/utils/settings.py
+++ core/data_requesting/utils/settings.py
@@ -7,7 +7,7 @@ from datetime import date
 # Web Request Defaults
 TRIES: int = 5
 FAIL_DELAY: int = 60
-SUCCESS_DELAY: int = 10
+SUCCESS_DELAY: float = 0.2
 
 # 17Lands Querying Defaults
 DEFAULT_FORMAT: str = 'PremierDraft'
