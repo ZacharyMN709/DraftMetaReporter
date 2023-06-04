d4702f1a8f149ffc15ebccaa8728b824dd38d280
Zachary Northcott <zmnorthcott@mun.ca>
2023-06-04 02:29:04 -0230

    Make `document_processing.py` atomic

--- Scripts/Document Templating/create_template.py
+++ Scripts/Document Templating/create_template.py
@@ -1,11 +1,89 @@
+import pandas as pd
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+from docx import Document
+
 import caching
 import tier_parsing
 import document_processing
 import config as cfg
 
 
+class DocumentCreator:
+    def __init__(
+        self,
+        grades,
+        left_margin=3.18,
+        right_margin=3.18,
+        top_margin=2.54,
+        bottom_margin=2.54
+    ):
+        self.card_count: int = 0
+        self.grades: pd.DataFrame = grades
+
+        self.left_margin: float = left_margin
+        self.right_margin: float = right_margin
+        self.top_margin: float = top_margin
+        self.bottom_margin: float = bottom_margin
+
+        self.document: Document = Document()
+        self.update_margins()
+
+    def update_margins(self):
+        document_processing.update_margins(
+            self.document,
+            top_margin=self.top_margin,
+            bottom_margin=self.bottom_margin,
+            left_margin=self.left_margin,
+            right_margin=self.right_margin
+        )
+
+    def add_card_to_document(self, card_name):
+        style = self.document.styles['Heading 3']
+        document_processing.apply_default_font(style.font)
+
+        heading = self.document.add_heading("", level=3)
+        heading.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+        run = heading.add_run(card_name)
+        card_data = caching.get_card_data(card_name)
+        card_url = card_data["scryfall_uri"]
+
+        document_processing.add_hyperlink(self.document, run, card_url)
+        table = self.document.add_table(rows=3, cols=4)
+        height, width = caching.download_card_image(card_name)
+        document_processing.add_image_to_cell(table.cell(0, 0), height, width, cfg.TEMP_LOC)
+        table.cell(0, 0).merge(table.cell(1, 2))
+
+        self.add_grade(table.cell(0, 3), card_name, 'Marc')
+        self.add_grade(table.cell(1, 3), card_name, 'Alex')
+
+        paragraph = table.cell(2, 0).paragraphs[0]
+        paragraph.style = 'List Bullet'
+        table.cell(2, 0).merge(table.cell(2, 3))
+
+        self.card_count += 1
+        if self.card_count % 2 == 0:
+            self.document.add_page_break()
+
+    def add_grade(self, cell, card_name, grader):
+        grade = self.grades.loc[card_name][grader].strip()
+        if grade.startswith('BA'):
+            grade = f"{grade[2:]}, \nBuild-Around"
+        if grade.startswith('SYN'):
+            grade = f"{grade[3:]}, \nSynergy"
+
+        cell.text = f"{grader}: {grade}"
+        p = cell.paragraphs[0]
+        p.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+        r = p.runs[0]
+        r.bold = True
+        document_processing.apply_font(r.font, size=12, color=(0x00, 0x00, 0x00))
+
+    def save_as(self, file_name):
+        self.document.save(file_name)
+
+
 def create_document(grades, card_names, doc_name="test.docx", hori_margin=3.18, vert_margin=2.54):
-    doc_handler = document_processing.DocumentCreator(
+    doc_handler = DocumentCreator(
         grades,
         left_margin=hori_margin,
         right_margin=hori_margin,
--- Scripts/Document Templating/document_processing.py
+++ Scripts/Document Templating/document_processing.py
@@ -1,11 +1,12 @@
-from docx import Document
 from docx.shared import Pt, RGBColor, Inches, Cm
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 import docx.opc.constants
 import docx.oxml.ns
 
-import config as cfg
-import caching
+# Font Defaults
+FONT_NAME = 'Calibri Light'
+FONT_SIZE = 16
+FONT_COLOR = 0x1F, 0x37, 0x63
 
 
 def set_cell_margins(cell, **kwargs):
@@ -39,9 +40,9 @@ def apply_default_font(font):
 
 
 def apply_font(font, name: str = None, size: int = None, color: tuple[int, int, int] = None):
-    font.name = name or cfg.FONT_NAME
-    font.size = Pt(size or cfg.FONT_SIZE)
-    font.color.rgb = RGBColor(*(color or cfg.FONT_COLOR))
+    font.name = name or FONT_NAME
+    font.size = Pt(size or FONT_SIZE)
+    font.color.rgb = RGBColor(*(color or FONT_COLOR))
 
 
 def update_margins(document, top_margin: float, bottom_margin: float, left_margin: float, right_margin: float):
@@ -70,76 +71,3 @@ def add_hyperlink(document, run, url: str, is_external: bool = True):
     run._r.getparent().replace(run._r, r_hyperlink)
     apply_default_font(run.font)
 
-
-class DocumentCreator:
-    def __init__(
-        self,
-        set_grades,
-        left_margin=3.18,
-        right_margin=3.18,
-        top_margin=2.54,
-        bottom_margin=2.54
-    ):
-        self.card_count = 0
-        self.set_grades = set_grades
-
-        self.left_margin = left_margin
-        self.right_margin = right_margin
-        self.top_margin = top_margin
-        self.bottom_margin = bottom_margin
-
-        self.document = Document()
-        self.update_margins()
-
-    def update_margins(self):
-        update_margins(
-            self.document,
-            top_margin=self.top_margin,
-            bottom_margin=self.bottom_margin,
-            left_margin=self.left_margin,
-            right_margin=self.right_margin
-        )
-
-    def add_card_to_document(self, card_name):
-        style = self.document.styles['Heading 3']
-        apply_default_font(style.font)
-
-        heading = self.document.add_heading("", level=3)
-        heading.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
-        run = heading.add_run(card_name)
-        card_data = caching.get_card_data(card_name)
-        card_url = card_data["scryfall_uri"]
-
-        add_hyperlink(self.document, run, card_url)
-        table = self.document.add_table(rows=3, cols=4)
-        height, width = caching.download_card_image(card_name)
-        add_image_to_cell(table.cell(0, 0), height, width, cfg.TEMP_LOC)
-        table.cell(0, 0).merge(table.cell(1, 2))
-
-        self.add_grade(table.cell(0, 3), card_name, 'Marc')
-        self.add_grade(table.cell(1, 3), card_name, 'Alex')
-
-        paragraph = table.cell(2, 0).paragraphs[0]
-        paragraph.style = 'List Bullet'
-        table.cell(2, 0).merge(table.cell(2, 3))
-
-        self.card_count += 1
-        if self.card_count % 2 == 0:
-            self.document.add_page_break()
-
-    def add_grade(self, cell, card_name, grader):
-        grade = self.set_grades.loc[card_name][grader].strip()
-        if grade.startswith('BA'):
-            grade = f"{grade[2:]}, \nBuild-Around"
-        if grade.startswith('SYN'):
-            grade = f"{grade[3:]}, \nSynergy"
-
-        cell.text = f"{grader}: {grade}"
-        p = cell.paragraphs[0]
-        p.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
-        r = p.runs[0]
-        r.bold = True
-        apply_font(r.font, size=12, color=(0x00, 0x00, 0x00))
-
-    def save_as(self, file_name):
-        self.document.save(file_name)
