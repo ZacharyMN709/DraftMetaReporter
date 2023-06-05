1d3e51a89396e2e89966f97c6baa1dd65116ae04
Zachary Northcott <zmnorthcott@mun.ca>
2023-06-05 00:33:35 -0230

    Add xlsx generation code

---
+++ Scripts/Document Templating/GenerateReviewSpreadsheet.py
@@ -0,0 +1,36 @@
+from typing import Any
+import pandas as pd
+
+from core.game_metadata import Card
+
+import card_ordering
+
+
+def gen_dict_from_card(card: Card, reviewers: list[str] = None) -> dict[str, Any]:
+    sanitized_name = card.NAME.replace('"', '')
+    mapping_dict = {
+        "Card Name": f'=HYPERLINK("{card.URL}", "{sanitized_name}")',
+        "Color": card.CAST_IDENTITY,
+        "Cost": card.MANA_COST,
+        "Rarity": card.RARITY,
+        "Type": " ".join(card.TYPES)
+    }
+
+    if reviewers:
+        for reviewer in reviewers:
+            mapping_dict[reviewer] = ""
+    return mapping_dict
+
+
+def gen_review_spreadsheet(file_name: str, set_code: str, reviewers: list[str] = None):
+    cards = card_ordering.get_set_order(set_code)
+    records = [gen_dict_from_card(card, reviewers) for card in cards]
+    frame = pd.DataFrame.from_records(records)
+    frame.to_excel(file_name)
+
+
+if __name__ == "__main__":
+    SET = "LTR"
+    FILE = f"{SET} Gradings.xlsx"
+    REVIEWERS = ["Alex", "Marc"]
+    gen_review_spreadsheet(FILE, SET, REVIEWERS)
