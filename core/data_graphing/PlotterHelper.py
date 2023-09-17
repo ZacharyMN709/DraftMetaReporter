521dbc14442f03131dfb514aea0546ea86d2e468
Zachary Northcott <zmnorthcott@mun.ca>
2023-09-17 21:21:15 -0230

    Reorganize functions

--- core/data_graphing/PlotterHelper.py
+++ core/data_graphing/PlotterHelper.py
@@ -2,7 +2,6 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as plot_dates
-import pandas as pd
 import seaborn as sns
 import dataframe_image as dfi
 
@@ -49,6 +48,38 @@ class PlotterHelper:  # pragma: no cover
             return color
     # endregion Color Handling
 
+    # region Helper Methods
+    def accredit(self, *, y=0.01, x=0.5):
+        plt.figtext(x, y, settings.ACCREDIT_STR, **settings.ACCREDIT_KWARGS)
+
+    def desc_note(self, colors=None, roll=1, *, y=0.95, x=0.5):
+        color_filter = f"Color Filter: {colors}"
+        roll_filter = f"Rolling Average: {roll} Days"
+
+        if colors and roll > 1:
+            txt = f"{roll_filter}\n{color_filter}"
+        elif colors:
+            txt = f"\n{color_filter}"
+        elif roll > 1:
+            txt = f"\n{roll_filter}"
+        else:
+            txt = ""
+
+        plt.figtext(x, y, txt, **settings.FILTER_KWARGS)
+
+    def set_x_axis_weekly(self, ax):
+        weeks = plot_dates.DayLocator(interval=7)
+        ax.xaxis.set_major_locator(weeks)
+        days = plot_dates.DayLocator(interval=1)
+        ax.xaxis.set_minor_locator(days)
+
+    def set_x_axis_daily(self, ax):
+        two_days = plot_dates.DayLocator(interval=2)
+        ax.xaxis.set_major_locator(two_days)
+        days = plot_dates.DayLocator(interval=1)
+        ax.xaxis.set_minor_locator(days)
+    # endregion Helper Methods
+
     def _get_sub_ax(self, x=None, y=None):
         if not isinstance(self.AX, np.ndarray):
             return self.AX
@@ -85,24 +116,6 @@ class PlotterHelper:  # pragma: no cover
         self.FIG.autofmt_xdate(rotation=45, ha='right')
         return self.FIG, self.AX
 
-    def accredit(self, *, y=0.01, x=0.5):
-        plt.figtext(x, y, settings.ACCREDIT_STR, **settings.ACCREDIT_KWARGS)
-
-    def desc_note(self, colors=None, roll=1, *, y=0.95, x=0.5):
-        color_filter = f"Color Filter: {colors}"
-        roll_filter = f"Rolling Average: {roll} Days"
-
-        if colors and roll > 1:
-            txt = f"{roll_filter}\n{color_filter}"
-        elif colors:
-            txt = f"\n{color_filter}"
-        elif roll > 1:
-            txt = f"\n{roll_filter}"
-        else:
-            txt = ""
-
-        plt.figtext(x, y, txt, **settings.FILTER_KWARGS)
-
     def set_labels(self, x_label="", y_label="", fontsize=settings.LABEL_SIZE, g_x=None, g_y=None):
         ax = self._get_sub_ax(g_x, g_y)
         ax.set_xlabel(x_label, fontsize=fontsize)
@@ -121,18 +134,6 @@ class PlotterHelper:  # pragma: no cover
         if inv_x:
             ax.invert_xaxis()
         ax.legend()
-
-    def set_x_axis_weekly(self, ax):
-        weeks = plot_dates.DayLocator(interval=7)
-        ax.xaxis.set_major_locator(weeks)
-        days = plot_dates.DayLocator(interval=1)
-        ax.xaxis.set_minor_locator(days)
-
-    def set_x_axis_daily(self, ax):
-        two_days = plot_dates.DayLocator(interval=2)
-        ax.xaxis.set_major_locator(two_days)
-        days = plot_dates.DayLocator(interval=1)
-        ax.xaxis.set_minor_locator(days)
 
     def save_fig(self, filename, sub_dir=None, dpi=None):
         if dpi is None:
