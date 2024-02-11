07d4d716ccad4efc00f12022d0ad8cffec5508fb
Zachary Northcott <zmnorthcott@mun.ca>
2024-02-10 22:29:58 -0330

    Add hyperlink formatting for card indxes

--- core/data_graphing/utils/funcs.py
+++ core/data_graphing/utils/funcs.py
@@ -4,6 +4,8 @@ import functools
 from pandas import DataFrame
 from pandas.io.formats.style import Styler
 
+from core.game_metadata import Card
+
 
 @functools.singledispatch
 def prettify_frame(arg) -> Union[Styler, NoReturn]:  # pragma: no cover
@@ -37,3 +39,27 @@ def _prettify_style(styler: Styler) -> Styler:  # pragma: no cover
     styler.set_table_styles([cell_hover, index_names, headers])
     styler.set_table_styles([{'selector': 'th.col_heading', 'props': 'text-align: left;'}], overwrite=False)
     return styler
+
+
+def hyperlink_card_index(card_name: str, card_dict: dict[str, Card]) -> str:
+    try:
+        card = card_dict[card_name]
+        html = (
+            f'<style>\n'
+            f'    .hover_img a {{ position:relative; }}\n'
+            f'    .hover_img a span {{ position:absolute; display:none; z-index:300; }}\n'
+            f'    .hover_img a:hover span {{ \n'
+            f'        display:block; height: 300px; width: 300px; overflow: visible; margin-left: 50px;\n'
+            f'     }}\n'
+            f'    .hover_img a:link {{ color: white; }}\n'
+            f'    .hover_img a:visited {{ color: white; }}\n'
+            f'</style>\n'
+            f'<div class="hover_img">\n'
+            f'    <a href="{card.URL}" target="_blank">{card.NAME}\n'
+            f'        <span><img src="{card.IMAGE_URL}" alt="image"/></span>\n'
+            f'    </a>\n'
+            f'</div>'
+        )
+        return html
+    except KeyError:
+        return card_name
